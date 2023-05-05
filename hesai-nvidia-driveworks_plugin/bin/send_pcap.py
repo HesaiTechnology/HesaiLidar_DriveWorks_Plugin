6445ddc34337639eedb9f7aab298581cf96dc721
xiaoleicai <xiaoleicai@hesaitech.com>
2023-05-05 18:44:05 +0800

    Add python script to simulate sending UDP packet from pcap

--- hesai-nvidia-driveworks_plugin/bin/send_pcap.py
+++ hesai-nvidia-driveworks_plugin/bin/send_pcap.py
@@ -1,29 +1,27 @@
-# -*- coding: utf-8 -*-
+# Licensed under the Apache License: Version 2.0
+# This file is used to simulate the sending of lidar UDP packet from the pcap file 
+# Refer to https://gist.github.com/ninedraft/7c47282f8b53ac015c1e326fffb664b5 for more information
+# Copyright (C) Hesai Technology Co., Ltd
 
 import socket
 import time
 from scapy.utils import PcapReader
 
-# https://gist.github.com/ninedraft/7c47282f8b53ac015c1e326fffb664b5
-
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
-# Enable broadcasting mode
+# enable broadcasting mode
 server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-# pcap_sample_p128 pcap_sample_qt128
+# support three types of lidar, pcap_sample_p128 pcap_sample_qt128 pcap_sample_qt128
 filename = r'../data/pcap_sample_p128.pcap'
 
 while (True):
     with PcapReader(filename) as packets:
         for packet in packets:
-            # print(p)
             server.sendto(packet["Raw"].load[42:], ('<broadcast>', 2368))
-            # break
-            # ensure the point cloud display clearly 1ms
+            # ensure the point cloud display, 1ms
             # time.sleep(0.001)
-    # break
-    print("----------")
+    print("-----Replay the pcap-----")
 

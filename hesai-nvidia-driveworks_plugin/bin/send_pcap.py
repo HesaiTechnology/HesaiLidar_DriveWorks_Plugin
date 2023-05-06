c5df587602ed895591c4deebdeb8d4ed9fa00746
xiaoleicai <xiaoleicai@hesaitech.com>
2023-05-06 15:18:05 +0800

    Add notes explain the reason why playing pcap slow down

--- hesai-nvidia-driveworks_plugin/bin/send_pcap.py
+++ hesai-nvidia-driveworks_plugin/bin/send_pcap.py
@@ -17,6 +17,8 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 # support three types of lidar, pcap_sample_p128 pcap_sample_qt128 pcap_sample_qt128
 filename = r'../data/pcap_sample_p128.pcap'
 
+# @note sending packets could be slow down while computing too large bunch of points, e.g. Pandar128
+# This phenomenent could be witnessed by PandarView & sample_lidar_replay
 while (True):
     with PcapReader(filename) as packets:
         for packet in packets:
